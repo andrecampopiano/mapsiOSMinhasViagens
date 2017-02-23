0c71f63b72682ea92b7e1d01629d0a8911e5d996
André Luís  Campopiano <andreluiscampopiano@mac-mini-de-andre.local>
2017-02-23 15:59:27 -0300

    incluindo annotation no mapa

---
+++ Minhas Viagens/Minhas Viagens/Cell/PlaceCell.swift
@@ -0,0 +1,25 @@
+//
+//  PlaceCell.swift
+//  Minhas Viagens
+//
+//  Created by André Luís  Campopiano on 23/02/17.
+//  Copyright © 2017 André Luís  Campopiano. All rights reserved.
+//
+
+import UIKit
+
+class PlaceCell: UITableViewCell {
+
+    @IBOutlet weak var lblPlace: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
