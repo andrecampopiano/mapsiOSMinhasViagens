8229ef90ecd54c14fff6df3f0a7c77469cbf0f08
André Luís  Campopiano <andreluiscampopiano@Mac-mini-de-Andre.local>
2017-02-23 13:10:46 -0300

    inicio do projeto

---
+++ Minhas Viagens/Minhas Viagens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Minhas Viagens
+//
+//  Created by André Luís  Campopiano on 23/02/17.
+//  Copyright © 2017 André Luís  Campopiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
